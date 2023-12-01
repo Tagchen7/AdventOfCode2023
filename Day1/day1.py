4351abe93b6fbda5f3e74c043fb3d26b901b1f03
Tagchen7 <140506078+Tagchen7@users.noreply.github.com>
2023-12-01 17:58:27 +0100

    day 1

---
+++ Day1/day1.py
@@ -0,0 +1,24 @@
+
+def main():
+    lines = get_input_str("input.txt")
+    numbers = []
+    for line in lines:
+        numbers.append(int("".join(get_first_num(line), get_first_num(line[::-1]))))
+    return sum(numbers)
+
+
+def get_first_num(line):
+    for char in line:
+        if char.isnumeric():
+            return char
+    return None
+
+def get_input_str(filename):
+    with open(filename) as f:
+        line = True
+        lines = []
+        while line:
+            line = f.readline()
+            lines.append(line)
+    f.close()
+    return lines
