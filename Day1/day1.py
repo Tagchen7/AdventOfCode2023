19038a8ec7f95d554ef741430c4ede38965fb008
Tagchen7 <140506078+Tagchen7@users.noreply.github.com>
2023-12-01 19:48:33 +0100

    only use digits

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,7 +1,7 @@
 import os
 
-NUMBERS = {"ten":"10", "eleven":"11", "twelve":"12", "thirteen":"13", "fourteen":"14", "fifteen":"15", "sixteen":"16", "seventeen":"17", "eighteen":"18", "nineteen":"19", "twenty":"20", "thirty":"30", "fourty":"40", "fifty":"50", "sixty":"60", "seventy":"70", "eighty":"80", "ninety":"90", "hundred":"100"}
-DIGITS = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+NUMBERS = {"zero":"0", "ten":"10", "eleven":"11", "twelve":"12", "thirteen":"13", "fourteen":"14", "fifteen":"15", "sixteen":"16", "seventeen":"17", "eighteen":"18", "nineteen":"19", "twenty":"20", "thirty":"30", "fourty":"40", "fifty":"50", "sixty":"60", "seventy":"70", "eighty":"80", "ninety":"90", "hundred":"100"}
+DIGITS = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
 
 def main(option=1):
     lines = get_input_str("input.txt")
@@ -19,6 +19,7 @@ def add_letter(line):
     idx_last = -1
     last = ""
     
+    """
     for key, value in NUMBERS.items():
         i = line.find(key)
         if idx_first == -1 or (i != -1 and i < idx_first):
@@ -28,7 +29,7 @@ def add_letter(line):
         if i > idx_last:
             idx_last = j
             last = value
-
+    """
     for key, value in DIGITS.items():
         i = line.find(key)
         if idx_first == -1 or (i != -1 and i < idx_first):
