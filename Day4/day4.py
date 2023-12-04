757e485c83a77f20ae51f7e1acf6fcec749c8bfd
Tagchen7 <140506078+Tagchen7@users.noreply.github.com>
2023-12-04 20:46:26 +0100

    finished day4 all parts

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,19 +1,39 @@
 import os
 
-def main():
-    lines = get_input("text.txt")
-    info = []
+def main(option):
+    lines = get_input("input.txt")
+    info = {}
+    amount = {}
     for line in lines:
-        [_, temp] = line.split(":")
+        [game, temp] = line.split(":")
+        game = int(game.split(" ")[-1])
         temp = temp.split("|")
         temp[0] = temp[0].split(" ")
         temp[1] = temp[1].split(" ")
+        for t in temp[0]:
+            t = t.strip()
+        for t in temp[1]:
+            t = t.strip()
+
         cntwin = 0
         for num in temp[1]:
-            if num in temp[0]:
-                cntwin += 1
-        info.append(min(cntwin, 1)*2**cntwin)
-    return sum(info)
+            if num != "" and num in temp[0]:
+                if option == 1:
+                    cntwin = max(cntwin+1, cntwin*2)
+                else:
+                    cntwin += 1
+        info[game] = cntwin
+        amount[game] = 1
+    
+    for key, value in info.items():
+        cnt = value
+        while cnt != 0:
+            amount[key+cnt] += amount[key]
+            cnt -= 1
+
+    if option == 1:
+        return sum(info.values())
+    return sum(amount.values())
 
 def get_input(file_name):
     # find filepath of txt file from this file
@@ -23,4 +43,4 @@ def get_input(file_name):
     with open(file_path, "r") as file:
         return [x.strip() for x in file]
     
-print(main())
+print(main(2))
